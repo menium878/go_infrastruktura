769f815b1af191a61d2a40cfd73ea331b5838982
unknown <meus13@gmail.com>
2023-03-16 22:29:47 +0100

    HTML and stuff

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func init() {
 }
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
