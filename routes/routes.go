100e2096786c8534f04385d4cf3910ded23952a2
unknown <meus13@gmail.com>
2023-04-10 23:17:47 +0200

    DB stuff

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,8 @@ import (
 func ImageUpload(c *gin.Context) {
 	// ? dowiedz się jak będzie działało jeśli wrzucisz w cloud i potem zapis tego zdjęcia
 	// !popisać timeoutty contexty
+	// !Dopisać wywołanie pytanie o maila i podpisz zdjęcie nim potem wyciągniemy sobie z DB mail po nazwie zdjęcia
+	// ?Przemyśleć czy może logowanie i rejestracja wtedy punkt wyżej działa inaczej
 	// multiple files
 	form, err := c.MultipartForm()
 	if err != nil {
