844e5b83e87793fe00592543791c4eb2f424108a
unknown <meus13@gmail.com>
2023-04-19 20:37:01 +0200

    wstepnial model

--- main.py
+++ main.py
@@ -70,3 +70,23 @@ print(data_df)
 
 
 # !!! Sprawdzić wypisywanie
+
+class Mish(tf.keras.layers.Layer):
+    
+    def __init__(self, **kwargs):
+        super(Mish, self).__init__(**kwargs)
+        self.supports_masking = True
+
+    def call(self, inputs):
+        return inputs * K.tanh(K.softplus(inputs))
+
+    def get_config(self):
+        base_config = super(Mish, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+def mish(x):
+    return tf.keras.layers.Lambda(lambda x: x*K.tanh(K.softplus(x)))(x)
+ 
+get_custom_objects().update({'mish': Activation(mish)})
